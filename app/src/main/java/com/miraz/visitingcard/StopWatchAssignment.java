0f18e682c8a829ef341363c006d2f4303796017c
miraz <miraz.anik@gmail.com>
2022-11-27 14:29:20 +0600

    Update for Assignment 5

--- app/src/main/java/com/miraz/visitingcard/StopWatchAssignment.java
+++ app/src/main/java/com/miraz/visitingcard/StopWatchAssignment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Locale;
 
@@ -23,6 +24,8 @@ public class StopWatchAssignment extends AppCompatActivity {
             running = savedInstanceState.getBoolean("running");
             wasRunning = savedInstanceState.getBoolean("wasRunning");
         }
+
+        Toast.makeText(this,"On Create",Toast.LENGTH_LONG).show();
         runTimer();
     }
 
@@ -35,8 +38,15 @@ public class StopWatchAssignment extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"On Start",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
+        Toast.makeText(this,"On Resume",Toast.LENGTH_LONG).show();
         if (wasRunning) {
             running = true;
         }
@@ -45,8 +55,27 @@ public class StopWatchAssignment extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        Toast.makeText(this,"On Pause",Toast.LENGTH_LONG).show();
         wasRunning = running;
         running = false;
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"On Restart",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"On Stop",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"On Destroy",Toast.LENGTH_LONG).show();
     }
 
     //Start the stopwatch running when the Start button is clicked.
